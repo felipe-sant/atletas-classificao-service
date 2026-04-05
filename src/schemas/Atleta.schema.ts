1380ab5aec011890a3a8c0c2d542e567c64721d1
Luiz Felipe <felipe.pinghang2@gmail.com>
2026-04-05 20:19:14 -0300

    refactor: mudando identacao

--- src/schemas/Atleta.schema.ts
+++ src/schemas/Atleta.schema.ts
@@ -1,17 +1,17 @@
 import { z } from "zod";
 
 export const atletaSchema = z.object({
-  Group: z.string().optional(),
-  Workload: z.number(),
-  "Sprint Distance": z.number(),
-  "High Intensity Running": z.number(),
-  "Top Speed": z.number(),
-  Accelerations: z.number(),
-  Decelerations: z.number(),
-  "No. of Sprints": z.number(),
-  "Metres per Minute": z.number(),
-  "No. of High Intensity Events": z.number(),
-  "Minutes Played": z.number(),
+    Group: z.string().optional(),
+    Workload: z.number(),
+    "Sprint Distance": z.number(),
+    "High Intensity Running": z.number(),
+    "Top Speed": z.number(),
+    Accelerations: z.number(),
+    Decelerations: z.number(),
+    "No. of Sprints": z.number(),
+    "Metres per Minute": z.number(),
+    "No. of High Intensity Events": z.number(),
+    "Minutes Played": z.number(),
 });
 
 export type Atleta = z.infer<typeof atletaSchema>;
